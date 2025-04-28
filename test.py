730a0035746b774981f64b5db14e1e6918c30648
Wy <3388038370@qq.com>
2025-04-28 18:19:24 +0800

    1

--- test.py
+++ test.py
@@ -11,10 +11,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="ReID Baseline Training")
     parser.add_argument(
         "--config_file", default="configs/OCC_Duke/vit_224.yml", help="path to config file", type=str
-        #"--config_file", default="configs/OCC_Duke/swin_OAFR.yml", help="path to config file", type=str
         #"--config_file", default="configs/OCC_REID/vit_224.yml", help="path to config file", type=str
         #"--config_file", default="configs/Partial_REID/vit_224.yml", help="path to config file", type=str
-        #"--config_file", default = "configs/Market/vit_oafr.yml", help = "path to config file", type = str
+
     )
     parser.add_argument("opts", help="Modify config options using the command-line", default=None,
                         nargs=argparse.REMAINDER)
